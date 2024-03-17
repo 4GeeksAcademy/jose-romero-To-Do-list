7d7ab3f336f8f85121e1399c2bd184b6a100d5c3
Jose Luis JoeDev <95258755+Jose890403@users.noreply.github.com>
2024-03-17 00:59:42 +0000

    utilizando fetch en el todo-list

--- src/js/component/TodoList.jsx
+++ src/js/component/TodoList.jsx
@@ -1,9 +1,67 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import TodoItem from './TodoItem';
 import "../../styles/todo.css"
 
 const TodoList = () => {
+{/*este es el primer fetch  donde se pide la informacion necesaria para ingresara a la api y si no crearla */}
+  const informacion =()=>{
+    fetch(`https://playground.4geeks.com/apis/fake/todos/user/Diana024`,{
+      method : "GET"
+    })
+    .then(devolverInfo => {
+      console.log(devolverInfo.ok);
+      console.log(devolverInfo.status);
+      if( devolverInfo.status === 404){
+        console.log("quieres crear un nuevo usuario")
+        createUser()
+      }
+      return devolverInfo.json;
+    })
 
+    .then(data =>{
+      console.log(data);
+      setTodos(data)
+    })
+    .catch(function (error) {
+      console.log("Hubo un problema con la petición Fetch:" + error);
+    });
+  }
+{/* aqui va el methodo post para pedir la creacion del usuairo*/}
+
+const crearUsuario = ()=>{
+    fetch(`https://playground.4geeks.com/apis/fake/todos/user/Diana024`,{
+      method:"POST",
+      headers :{
+        "content-type" : "application/Json "
+      },
+      body:JSON.stringify ([])
+    })
+    .then (crear => {
+      console.log(crear.ok);
+      console.log(crear.status);
+      return crear.json
+    })
+    .then(data =>{
+      console.log(data);
+    })
+    .catch(function (error) {
+      console.log("Hubo un problema con la petición Fetch:" + error);
+    });
+}
+
+{/* aqui va el methodo update*/}
+
+const actualizar = () =>{
+  fetch(`https://playground.4geeks.com/apis/fake/todos/user/Diana024`,{
+    method: "PUT",
+    body: [
+           { label: "Make the bed", done: false },
+           { label: "Walk the dog", done: false },
+           { label: "Do the replits", done: false },
+          ]
+    })
+  }
+  
   const [todos, setTodos] = useState([]);
   const [newTodo, setNewTodo] = useState('');
 
@@ -20,6 +78,14 @@ const TodoList = () => {
     setTodos(updatedTodos);
   };
 
+    useEffect(() => {
+      crearUsuario();
+    })
+
+    useEffect(() => {
+      actualizar();
+    },[todos])
+
   return (
     <div className='contenedor'>
       <h1 className='text'>Todo List</h1>
@@ -30,6 +96,7 @@ const TodoList = () => {
           value={newTodo}
           onChange={(e) => setNewTodo(e.target.value)}
           placeholder='Tu tarea aqui'
+          onKeyDown={(e) => console.log(e)}
         />
       
         <button className='btn' onClick={addTodo}>Agregar To-Do</button>
